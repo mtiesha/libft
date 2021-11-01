dd35e8e220aa6e78e9387156f6a9b9f59d8c0556
mtiesha <86960854+mtiesha@users.noreply.github.com>
2021-11-01 08:56:21 +0300

    extract '-'

--- ft_atoi.c
+++ ft_atoi.c
@@ -14,29 +14,27 @@
 
 int	ft_atoi(const char *st)
 {
-	int			i;
-	int			s;
-	long long	res;
+	unsigned int			i;
+	int						s;
+	long long				res;
 
 	i = 0;
 	s = 1;
 	res = 0;
-	while (st[i] != '\0' && (st[i] == '+'
-			|| st[i] == '-' || st[i] == ' '
-			|| st[i] == '\t' || st[i] == '\n'
-			|| st[i] == '\r' || st[i] == '\v')
+	while (st[i] != '\0' && (st[i] == 32
+			|| st[i] == ' ' || st[i] == '\t'
+			|| st[i] == '\n' || st[i] == '\r' 
+			|| st[i] == '\v')
 		i++;
-	if (st[i] == '-')
-		s = -1;
-	while (ft_isdigit(st[i]))
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if (res == 214748364 && ((st[i] - '0') > 8))
-			retutn (-1);
-		if (res == 214748364 && ((st[i] - '0') > 7))
-			retutn (0);
-		res = res * 10 + (st[i++] - '0');
+		if (str[i] == '-')
+			sign = -1;
+		i++;
 	}
+	while (ft_isdigit(st[i]))
+		res = res * 10 + (st[i++] - '0');
 	if (res != 0)
-		return (res * s);
+		return ((int)(res * s));
 	return (0);
 }
