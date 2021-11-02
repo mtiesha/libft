135f6e1bc298ca20b35e19ab4f7ce8c483fe57ea
mtiesha <86960854+mtiesha@users.noreply.github.com>
2021-11-02 12:17:23 +0300

    fixed

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,19 +6,28 @@
 /*   By: mtiesha <mtiesha@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 17:57:59 by mtiesha           #+#    #+#             */
-/*   Updated: 2021/10/26 19:16:32 by mtiesha          ###   ########.fr       */
+/*   Updated: 2021/11/02 19:16:32 by mtiesha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size)
 {
 	size_t	src_len;
 
-	src_len = ft_strlen(src);
 	if (!dst || !src)
 		return (0);
-	ft_memcpy(dst, src, size);
-	return (size);
+	src_len = ft_strlen(src);
+	if (!dst_size)
+		return (src_len);
+	if (dst_size > src_len + 1)
+		ft_memcpy(dst, src, src_len);
+	else if (dst_size <= src_len + 1)
+		ft_memcpy(dst, src, dst_size - 1);
+	if (dst_size > src_len + 1)
+		dst[src_len] = 0;
+	else if (dst_size <= src_len + 1)
+		dst[dst_size - 1] = 0;
+	return (src_len);
 }
