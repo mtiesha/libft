0205d7c5e83d316575ed731eebb5fa2d1940416a
mtiesha <86960854+mtiesha@users.noreply.github.com>
2021-11-03 12:13:28 +0300

    ver 2
    not work

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,42 +6,60 @@
 /*   By: mtiesha <mtiesha@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 15:53:02 by mtiesha           #+#    #+#             */
-/*   Updated: 2021/10/26 19:16:32 by mtiesha          ###   ########.fr       */
+/*   Updated: 2021/11/03 19:16:32 by mtiesha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_concaten(char *dest, const char *src, size_t dest_size)
+static size_t	ft_find_zero(const char *string, size_t n)
 {
-	dest = ft_memchr(dest, '\0', dest_size);
-	while (*src++)
-		*dest++ = src[-1];
-	dest = 0;
-}
-
-size_t	ft_strlcat(char *dest, const char *src, size_t dest_size)
-{
-	size_t			src_dest_len;
-	int				i;
-	unsigned long	dest_len;
+	int	i;
 
 	i = 0;
-	src_dest_len = ft_strlen(src) + ft_strlen(dest);
-	if (!dest || !src)
-		return (0);
-	if (!dest_size)
-		return (src_dest_len);
-	if (dest_size >= src_dest_len + 1)
-		ft_concaten(dest, src, dest_size);
-	else
+	while (string[i] != '\0' && n)
 	{
-		dest_len = ft_strlen(dest) + 1;
-		dest = ft_memchr(dest, '\0', dest_size);
-		dest_size = dest_size - dest_len;
-		while (dest_size--)
-			*dest++ = *src++;
-		dest = 0;
+		i++;
+		n--;
 	}
-	return (src_dest_len);
+	if (string[i] != '\0')
+		return (0);
+	return (i);
+}
+
+static size_t	ft_concaten(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (dest[i] != 0)
+		i++;
+	while (n - 1)
+	{
+		dest[i++] = *src++;
+		n--;
+	}
+	dest[i] = 0;
+	return (i);
+}
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	dest_len;
+	size_t	src_len;
+
+	src_len = ft_strlen(src);
+	if (!size)
+		return (src_len);
+	dest_len = ft_find_zero(dest, size);
+	if (!dest_len)
+	{
+		while (size--)
+			*dest++ = *src++;
+		return (src_len + size);
+	}
+	if (size > dest_len)
+		return (ft_concaten(dest, src, size - 1));
+	else
+		return (ft_concaten(dest, src, size - dest_len - 1));
 }
