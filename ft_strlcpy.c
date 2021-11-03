eadfe176eef196a9b422ff20709748647de29b6a
mtiesha <86960854+mtiesha@users.noreply.github.com>
2021-11-03 20:52:02 +0300

    version 2
    add seg

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -16,8 +16,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size)
 {
 	size_t	src_len;
 
-	if (!dst || !src)
-		return (0);
 	src_len = ft_strlen(src);
 	if (!dst_size)
 		return (src_len);
