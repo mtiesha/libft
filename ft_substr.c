a8fea09fe1a726195c704995ada061cf0a91958b
mtiesha <86960854+mtiesha@users.noreply.github.com>
2021-11-06 06:26:15 +0300

    Version 2
    fixed

--- ft_substr.c
+++ ft_substr.c
@@ -12,16 +12,19 @@
 
 #include "libft.h"
 
-char	*ft_substr(char const *str, unsigned int start, size_t len)
+char	*ft_substr(const char *str, unsigned int start, size_t len)
 {
 	unsigned char	*substr;
 
+	if (!str || !len)
+		return (NULL);
+	if (start >= ft_strlen(str))
+		return (ft_strdup(""));
 	substr = (unsigned char *)malloc((len + 1) * sizeof(char));
 	if (substr != NULL)
 	{
-		str = str + start;
-		ft_memcpy(substr, str, len);
-		substr[len + 1] = 0;
+		ft_memcpy(substr, str + start, len);
+		substr[len] = 0;
 		return ((char *)(substr));
 	}
 	else
