31ea95eaf5ecdd0ed56e1c6d4d8e699a0519d3c7
mtiesha <86960854+mtiesha@users.noreply.github.com>
2021-11-07 17:52:46 +0300

    version 2

--- ft_striteri.c
+++ ft_striteri.c
@@ -17,6 +17,12 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 	unsigned int	i;
 
 	i = 0;
-	while (s[i++] != '\0')
-		(*f)(i, &s[i]);
+	if (s)
+	{
+		while (s[i] != '\0')
+		{
+			(*f)(i, &s[i]);
+			i++;
+		}
+	}
 }
