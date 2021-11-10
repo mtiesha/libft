dc520ce7862dd362e23ac369e3f470407c555ded
mtiesha <86960854+mtiesha@users.noreply.github.com>
2021-11-10 08:11:03 +0300

    version 2
    fix min int input

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,11 +14,15 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
 	if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
+		n = n * -1;
+	}
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('2', fd);
+		n = n % 1000000000;
 		n = n * -1;
 	}
 	if (n >= 10)
@@ -26,6 +30,6 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putnbr_fd(n / 10, fd);
 		ft_putnbr_fd(n % 10, fd);
 	}
-	if (n < 10)
-		ft_putchar_fd(n + 48, fd);
+	else
+		ft_putchar_fd(n + '0', fd);
 }
