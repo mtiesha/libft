41f0c140c350d779fb31fa3bef5c39f8f2cabb28
mtiesha <86960854+mtiesha@users.noreply.github.com>
2021-11-10 08:29:25 +0300

    version 1.2
    OK

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -14,11 +14,11 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	long long	i;
+	unsigned long	i;
 
 	i = ft_strlen(s) + 1;
-	while (i-- >= 0)
-		if (s[i] == (char)c)
+	while (i-- != 0)
+		if (s[i] == (char)(c))
 			return ((char *)(s + i));
 	return (0);
 }
