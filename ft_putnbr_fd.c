049af8edb59287a10e62199b6345e451f412b1e1
mtiesha <86960854+mtiesha@users.noreply.github.com>
2022-03-23 12:47:15 +0300

    add return (count char)

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -12,24 +12,27 @@
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+int	ft_putnbr_fd(int n, int fd)
 {
+	int	i;
+
+	i = 0;
 	if (n < 0)
 	{
-		ft_putchar_fd('-', fd);
+		i += ft_putchar_fd('-', fd);
 		n = n * -1;
 	}
 	if (n == -2147483648)
 	{
-		ft_putchar_fd('2', fd);
+		i += ft_putchar_fd('2', fd);
 		n = n % 1000000000;
 		n = n * -1;
 	}
 	if (n >= 10)
 	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		i += ft_putnbr_fd(n / 10, fd);
+		i += ft_putnbr_fd(n % 10, fd);
 	}
 	else
-		ft_putchar_fd(n + '0', fd);
+		i += ft_putchar_fd(n + '0', fd);
 }
